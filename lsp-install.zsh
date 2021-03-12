8588b03eeaed15897067b1e4a4e6e63391899433
Ryan Nevius <ryanevius@gmail.com>
2021-03-12 22:11:41 +0100

    Update lsp-install script. Add lua server support.

--- lsp-install.zsh
+++ lsp-install.zsh
@@ -1,26 +1,60 @@
 #!/bin/zsh
 
 # CSS
+echo "\n Installing CSS server...\n"
 npm install -g vscode-css-languageserver-bin
 
-# HTML
-npm install -g vscode-html-languageserver-bin
-
-# JavaScript / TypeScript
-npm install -g typescript typescript-language-server
-
-# JSON
-npm install -g vscode-json-languageserver
-
 # Elixir
+echo "\n Installing Elixir server...\n"
 curl --show-error --location https://github.com/elixir-lsp/elixir-ls/releases/latest/download/elixir-ls.zip | tar xf - -C $HOME/.elixir-ls/release/
 chmod u+x $HOME/.elixir-ls/release/language_server.sh
 
+# Go
+echo "\n Installing Go server...\n"
+if which gopls > /dev/null 2>&1
+then
+  brew upgrade gopls
+else
+  brew install gopls
+fi
+
+# HTML
+echo "\n Installing HTML server...\n"
+npm install -g vscode-html-languageserver-bin
+
+# JavaScript / TypeScript
+echo "\n Installing TypeScript server...\n"
+npm install -g typescript typescript-language-server
+
+# JSON
+echo "\n Installing JSON server...\n"
+npm install -g vscode-json-languageserver
+
+# Lua
+echo "\n Installing Lua server...\n"
+if which ninja > /dev/null 2>&1
+then
+  brew upgrade ninja
+else
+  brew install ninja
+fi
+rm -rf $HOME/.lua-ls
+git clone https://github.com/sumneko/lua-language-server $HOME/.lua-ls
+cd $HOME/.lua-ls
+git submodule update --init --recursive
+cd 3rd/luamake
+ninja -f ninja/macos.ninja
+cd ../..
+./3rd/luamake/luamake rebuild
+
 # Python
+echo "\n Installing Python server...\n"
 pip3 install python-language-server pyls-black
 
 # Ruby
+echo "\n Installing Ruby server...\n"
 gem install --user-install solargraph
 
 # YAML
+echo "\n Installing YAML server...\n"
 npm install -g yaml-language-server
--- nvim/.config/nvim/lua/lsp-config.lua
+++ nvim/.config/nvim/lua/lsp-config.lua
@@ -1,5 +1,14 @@
 local nvim_lsp = require('lspconfig')
 
+local system_name
+if vim.fn.has('mac') == 1 then
+  system_name = 'macOS'
+elseif vim.fn.has('unix') == 1 then
+  system_name = 'Linux'
+elseif vim.fn.has('win32') == 1 then
+  system_name = 'Windows'
+end
+
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
@@ -30,9 +39,6 @@ local on_attach = function(client, bufnr)
   -- Set autocommands conditional on server_capabilities
   if client.resolved_capabilities.document_highlight then
     vim.api.nvim_exec([[
-      highlight link LspReferenceRead Visual
-      highlight link LspReferenceText Visual
-      highlight link LspReferenceWrite Visual
       augroup lsp_document_highlight
         autocmd!
         autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
@@ -43,7 +49,7 @@ local on_attach = function(client, bufnr)
   print("[" .. client.name .. "] client loaded!")
 end
 
--- Use a loop to conveniently both setup defined servers 
+-- Use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
 local servers = {
   "cssls",
@@ -62,13 +68,46 @@ for _, lsp in ipairs(servers) do
         vim.lsp.diagnostic.on_publish_diagnostics, {
           signs = true,
           underline = true,
-          virtual_text = false
+          -- virtual_text = false
         }
       ),
     },
     on_attach = on_attach
   }
 end
+
 nvim_lsp.elixirls.setup{
   cmd = { vim.env.HOME .. "/.elixir-ls/release/language_server.sh" };
+  on_attach = on_attach
 }
+
+-- set the path to the sumneko (lua lsp) installation
+local sumneko_root_path = vim.env.HOME .. '/.lua-ls'
+local sumneko_binary = sumneko_root_path .. '/bin/' .. system_name .. '/lua-language-server'
+
+require'lspconfig'.sumneko_lua.setup {
+  cmd = {sumneko_binary, '-E', sumneko_root_path .. '/main.lua'};
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = vim.split(package.path, ';'),
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+        },
+      },
+    },
+  },
+  on_attach = on_attach
+}
+
