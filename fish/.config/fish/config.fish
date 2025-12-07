cd089441e9dba094c72de8b1ae3122ba9ba5972a
Ryan Nevius <ryanevius@gmail.com>
2025-12-07 12:05:23 +0100

    chore(fish): better spec for interactive sessions

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -33,14 +33,6 @@ function __history_previous_command_arguments
   end
 end
 
-if [ "$fish_key_bindings" = fish_vi_key_bindings ];
-  bind -Minsert ! __history_previous_command
-  bind -Minsert '$' __history_previous_command_arguments
-else
-  bind ! __history_previous_command
-  bind '$' __history_previous_command_arguments
-end
-
 ## Variables
 set -gx EDITOR nvim
 # set -gx MANPAGER 'nvim +Man!'
@@ -49,64 +41,8 @@ set -x MANROFFOPT "-c"
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
 # Set settings for done
-set -U __done_min_cmd_duration 10000
-set -U __done_notification_urgency_level low
-
-## Abbreviations / Aliases
-alias ...='cd ../..'
-alias ....='cd ../../..'
-abbr -a jctl="journalctl -p 3 -xb"
-alias wget='wget -c '
-
-alias grep='grep --color=auto'
-
-# Git
-abbr -a g git
-abbr -a ga 'git add'
-abbr -a gaa 'git add --all'
-abbr -a gau 'git add --update'
-abbr -a gb 'git branch'
-abbr -a gbD 'git branch --delete --force'
-abbr -a gbd 'git branch --delete'
-abbr -a gc 'git commit --verbose'
-abbr -a gcB 'git checkout -B'
-abbr -a gcb 'git checkout -b'
-abbr -a gclean 'git clean --interactive -d'
-abbr -a gcm 'git checkout (git_main_branch)'
-abbr -a gco 'git checkout'
-abbr -a gd 'git diff'
-abbr -a gds 'git diff --staged'
-abbr -a gl 'git pull'
-abbr -a glgg 'git log --graph'
-abbr -a glgga 'git log --graph --decorate --all'
-abbr -a glgm 'git log --graph --max-count=10'
-abbr -a glo 'git log --oneline --decorate'
-abbr -a glog 'git log --oneline --decorate --graph'
-abbr -a gloga 'git log --oneline --decorate --graph --all'
-abbr -a glol 'git log --graph --pretty="%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset"'
-abbr -a gmod "git status --porcelain | awk '{ print \$2 }'"
-abbr -a gp 'git push'
-abbr -a gpr 'git pull --rebase'
-abbr -a gpsup 'git push --set-upstream origin $(git_current_branch)'
-abbr -a gst 'git status'
-
-# Lazy
-abbr -a lz lazygit
-abbr -a lzd lazydocker
-
-# ls / eza
-abbr -a ls eza
-abbr -a lsa 'eza --all'
-abbr -a lsl 'eza -lha --no-permissions --smart-group --time-style "relative"'
-abbr -a tree 'eza --tree'
-
-# Neovim
-abbr -a vi nvim
-abbr -a vim nvim
-abbr -a vimdiff 'nvim -d'
-
-# Pacman
-abbr -a cleanup='sudo pacman -Rns (pacman -Qtdq)'
+set -g __done_min_cmd_duration 10000
+set -g __done_notification_urgency_level low
 
 ## Colima
 if test -S $HOME/.colima/default/docker.sock
@@ -116,9 +52,78 @@ end
 
 ## Homebrew
 if test -x /opt/homebrew/bin/brew
-    eval "$(/opt/homebrew/bin/brew shellenv)" # add brew to PATH
+    eval (/opt/homebrew/bin/brew shellenv) # add brew to PATH
 end
-starship init fish | source
-mise activate fish | source
-fzf --fish | source
-zoxide init fish | source
+
+if status is-interactive
+    if test "$fish_key_bindings" = fish_vi_key_bindings
+      bind -Minsert ! __history_previous_command
+      bind -Minsert '$' __history_previous_command_arguments
+    else
+      bind ! __history_previous_command
+      bind '$' __history_previous_command_arguments
+    end
+
+    ## Abbreviations / Aliases
+    alias ...='cd ../..'
+    alias ....='cd ../../..'
+    abbr -a jctl 'journalctl -p 3 -xb'
+    alias wget='wget -c '
+
+    alias grep='grep --color=auto'
+
+    # Git
+    abbr -a g git
+    abbr -a ga 'git add'
+    abbr -a gaa 'git add --all'
+    abbr -a gau 'git add --update'
+    abbr -a gb 'git branch'
+    abbr -a gbD 'git branch --delete --force'
+    abbr -a gbd 'git branch --delete'
+    abbr -a gc 'git commit --verbose'
+    abbr -a gcB 'git checkout -B'
+    abbr -a gcb 'git checkout -b'
+    abbr -a gclean 'git clean --interactive -d'
+    abbr -a gcm 'git checkout (git_main_branch)'
+    abbr -a gco 'git checkout'
+    abbr -a gd 'git diff'
+    abbr -a gds 'git diff --staged'
+    abbr -a gl 'git pull'
+    abbr -a glgg 'git log --graph'
+    abbr -a glgga 'git log --graph --decorate --all'
+    abbr -a glgm 'git log --graph --max-count=10'
+    abbr -a glo 'git log --oneline --decorate'
+    abbr -a glog 'git log --oneline --decorate --graph'
+    abbr -a gloga 'git log --oneline --decorate --graph --all'
+    abbr -a glol 'git log --graph --pretty="%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset"'
+    abbr -a gmod "git status --porcelain | awk '{ print \$2 }'"
+    abbr -a gp 'git push'
+    abbr -a gpr 'git pull --rebase'
+    abbr -a gpsup 'git push --set-upstream origin (git_current_branch)'
+    abbr -a gst 'git status'
+
+    # Lazy
+    abbr -a lz lazygit
+    abbr -a lzd lazydocker
+
+    # ls / eza
+    abbr -a ls eza
+    abbr -a lsa 'eza --all'
+    abbr -a lsl 'eza -lha --no-permissions --smart-group --time-style "relative"'
+    abbr -a tree 'eza --tree'
+
+    # Neovim
+    abbr -a vi nvim
+    abbr -a vim nvim
+    abbr -a vimdiff 'nvim -d'
+
+    # Pacman
+    if type -q pacman
+        abbr -a cleanup 'sudo pacman -Rns (pacman -Qtdq)'
+    end
+
+    starship init fish | source
+    mise activate fish | source
+    fzf --fish | source
+    zoxide init fish | source
+end
