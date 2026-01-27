6cd5dc94c574a1f52bcf11591f7fa704991d2040
Ryan Nevius <ryanevius@gmail.com>
2026-01-27 10:13:14 +0100

    chore(nvim): better competion and autocommands

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,11 @@
 -- Main entry point for Neovim configuration
 
--- Set <space> as the leader key
--- NOTE: Must happen before plugins are loaded
+-- Define config table to be able to pass data between scripts
+_G.Config = {}
 
--- Load core configuration
--- require 'core'
-
+-- Define custom autocommand group and helper to create an autocommand.
+local gr = vim.api.nvim_create_augroup('custom-config', {})
+_G.Config.new_autocmd = function(event, pattern, callback, desc)
+  local opts = { group = gr, pattern = pattern, callback = callback, desc = desc }
+  vim.api.nvim_create_autocmd(event, opts)
+end
